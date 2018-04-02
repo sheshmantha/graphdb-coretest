bb4308a3844dac87706ed5aba5d501a5a9839975
Sheshadri Mantha <sheshadri.mantha@gmail.com>
2018-04-01 20:47:28 -0400

    Fixed unit test for creating a vertex; tested against local cassandra. Updated schema creation.

--- src/main/java/com/philips/graphdb/janus/JanusGraphDB.java
+++ src/main/java/com/philips/graphdb/janus/JanusGraphDB.java
@@ -31,47 +31,51 @@ public final class JanusGraphDB {
     private void makeSchema(JanusGraph g) {
         /* Schema is derived from: https://academy.datastax.com/content/gremlin-traversals */
         JanusGraphManagement mgmt = graph.openManagement();
-        // Property keys 
-        PropertyKey genreIdKey = mgmt.makePropertyKey("genreId").dataType(String.class).make();
-        PropertyKey personIdKey = mgmt.makePropertyKey("personId").dataType(String.class).make();
-        PropertyKey userIdKey = mgmt.makePropertyKey("userId").dataType(String.class).make();
-        PropertyKey movieIdKey = mgmt.makePropertyKey("movieId").dataType(String.class).make();
-        PropertyKey nameKey = mgmt.makePropertyKey("name").dataType(String.class).make();
-        PropertyKey ageKey = mgmt.makePropertyKey("age").dataType(Integer.class).make();
-        mgmt.makePropertyKey("gender").dataType(String.class).make();
-        PropertyKey titleKey = mgmt.makePropertyKey("title").dataType(String.class).make();
-        PropertyKey yearKey = mgmt.makePropertyKey("year").dataType(Integer.class).make();
-        mgmt.makePropertyKey("duration").dataType(Integer.class).make();
-        PropertyKey countryKey = mgmt.makePropertyKey("country").dataType(String.class).make();
-        mgmt.makePropertyKey("production").dataType(String.class).cardinality(Cardinality.LIST).make();
-        mgmt.makePropertyKey("rating").dataType(Integer.class).make();
+        if (mgmt.getVertexLabel("genre") == null) {
+            // Property keys
+            PropertyKey genreIdKey = mgmt.makePropertyKey("genreId").dataType(String.class).make();
+            PropertyKey personIdKey = mgmt.makePropertyKey("personId").dataType(String.class).make();
+            PropertyKey userIdKey = mgmt.makePropertyKey("userId").dataType(String.class).make();
+            PropertyKey movieIdKey = mgmt.makePropertyKey("movieId").dataType(String.class).make();
+            PropertyKey nameKey = mgmt.makePropertyKey("name").dataType(String.class).make();
+            PropertyKey ageKey = mgmt.makePropertyKey("age").dataType(Integer.class).make();
+            mgmt.makePropertyKey("gender").dataType(String.class).make();
+            PropertyKey titleKey = mgmt.makePropertyKey("title").dataType(String.class).make();
+            PropertyKey yearKey = mgmt.makePropertyKey("year").dataType(Integer.class).make();
+            mgmt.makePropertyKey("duration").dataType(Integer.class).make();
+            PropertyKey countryKey = mgmt.makePropertyKey("country").dataType(String.class).make();
+            mgmt.makePropertyKey("production").dataType(String.class).cardinality(Cardinality.LIST).make();
+            PropertyKey ratingKey = mgmt.makePropertyKey("rating").dataType(Integer.class).make();
 
 // Vertex labels
-        VertexLabel genre = mgmt.makeVertexLabel("genre").make(); // .properties("genreId","name").make();
-        VertexLabel person = mgmt.makeVertexLabel("person").make(); // .properties("personId","name").make();
-        VertexLabel user = mgmt.makeVertexLabel("user").make(); // .properties("userId","age","gender").make();
-        VertexLabel movie = mgmt.makeVertexLabel(VL_MOVIE).make(); // .properties("movieId","title","year","duration","country","production").make();
+            VertexLabel genre = mgmt.makeVertexLabel("genre").make(); // .properties("genreId","name").make();
+            VertexLabel person = mgmt.makeVertexLabel("person").make(); // .properties("personId","name").make();
+            VertexLabel user = mgmt.makeVertexLabel("user").make(); // .properties("userId","age","gender").make();
+            VertexLabel movie = mgmt.makeVertexLabel(VL_MOVIE).make(); // .properties("movieId","title","year","duration","country","production").make();
 
 // Edge labels
-        mgmt.makeEdgeLabel("knows").multiplicity(Multiplicity.MULTI).make(); // .connection("user","user").make();
-        EdgeLabel ratedEdge = mgmt.makeEdgeLabel("rated").multiplicity(Multiplicity.MULTI).make(); // single().properties("rating").connection("user","movie").make();
-        mgmt.makeEdgeLabel("belongsTo").multiplicity(Multiplicity.ONE2MANY).make(); // .single().connection("movie","genre").make();
-        mgmt.makeEdgeLabel("actor").multiplicity(Multiplicity.MULTI).make(); // .connection("movie","person").make();
-        mgmt.makeEdgeLabel("director").multiplicity(Multiplicity.MULTI).make(); // .single().connection("movie","person").make();
+            mgmt.makeEdgeLabel("knows").multiplicity(Multiplicity.MULTI).make(); // .connection("user","user").make();
+            EdgeLabel ratedEdge = mgmt.makeEdgeLabel("rated").multiplicity(Multiplicity.MULTI).make(); // single().properties("rating").connection("user","movie").make();
+            mgmt.makeEdgeLabel("belongsTo").multiplicity(Multiplicity.ONE2MANY).make(); // .single().connection("movie","genre").make();
+            mgmt.makeEdgeLabel("actor").multiplicity(Multiplicity.MULTI).make(); // .connection("movie","person").make();
+            mgmt.makeEdgeLabel("director").multiplicity(Multiplicity.MULTI).make(); // .single().connection("movie","person").make();
 
 // Vertex indexes
-        mgmt.buildIndex("genreByIdIdx", Vertex.class).addKey(genreIdKey).indexOnly(genre).buildCompositeIndex();
-        mgmt.buildIndex("genreByNameIdx", Vertex.class).addKey(nameKey).indexOnly(genre).buildCompositeIndex();
-        mgmt.buildIndex("personByIdIdx", Vertex.class).addKey(personIdKey).indexOnly(person).buildCompositeIndex();
-        mgmt.buildIndex("personByNameIdx", Vertex.class).addKey(nameKey).indexOnly(person).buildCompositeIndex();
-        mgmt.buildIndex("userByIdIdx", Vertex.class).addKey(userIdKey).indexOnly(user).buildCompositeIndex();
-        mgmt.buildIndex("userByAgeIdx", Vertex.class).addKey(ageKey).indexOnly(user).buildCompositeIndex();
+            mgmt.buildIndex("genreByIdIdx", Vertex.class).addKey(genreIdKey).indexOnly(genre).buildCompositeIndex();
+            mgmt.buildIndex("genreByNameIdx", Vertex.class).addKey(nameKey).indexOnly(genre).buildCompositeIndex();
+            mgmt.buildIndex("personByIdIdx", Vertex.class).addKey(personIdKey).indexOnly(person).buildCompositeIndex();
+            mgmt.buildIndex("personByNameIdx", Vertex.class).addKey(nameKey).indexOnly(person).buildCompositeIndex();
+            mgmt.buildIndex("userByIdIdx", Vertex.class).addKey(userIdKey).indexOnly(user).buildCompositeIndex();
+            mgmt.buildIndex("userByAgeIdx", Vertex.class).addKey(ageKey).indexOnly(user).buildCompositeIndex();
 
-        mgmt.buildIndex("moviesByIdIdx", Vertex.class).addKey(movieIdKey).indexOnly(movie).buildCompositeIndex();
-        mgmt.buildIndex("moviesByTitleIdx", Vertex.class).addKey(titleKey).indexOnly(movie).buildCompositeIndex();
+            mgmt.buildIndex("moviesByIdIdx", Vertex.class).addKey(movieIdKey).indexOnly(movie).buildCompositeIndex();
+            mgmt.buildIndex("moviesByTitleIdx", Vertex.class).addKey(titleKey).indexOnly(movie).buildCompositeIndex();
 // Edge indexes
-        mgmt.buildIndex("userRatingIdx", Edge.class).indexOnly(ratedEdge).buildCompositeIndex();
-        mgmt.commit();
+            mgmt.buildIndex("userRatingIdx", Edge.class).addKey(ratingKey).indexOnly(ratedEdge).buildCompositeIndex();
+            mgmt.commit();
+        }
+        else
+         mgmt.rollback();
     }
 
     public static final JanusGraph getInstance() {
--- src/test/java/com/philips/graphdb/janus/JanusGraphDBTest.java
+++ src/test/java/com/philips/graphdb/janus/JanusGraphDBTest.java
@@ -1,31 +1,37 @@
 package com.philips.graphdb.janus;
 
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
-import org.janusgraph.core.JanusGraph;
-import org.janusgraph.core.JanusGraphTransaction;
+import org.apache.tinkerpop.gremlin.structure.VertexProperty;
+import org.janusgraph.core.*;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.property;
 import static org.junit.Assert.*;
 
 /**
  * Created by shesh on 3/25/2018.
  */
 public class JanusGraphDBTest {
+    private JanusGraph graph;
+
+    @Before
+    public void setUp() throws Exception {
+        graph = JanusGraphDB.getInstance();
+    }
+
     @Test
-    public void getInstance() throws Exception {
-        JanusGraph graph = JanusGraphDB.getInstance();
-//        JanusGraphTransaction tx = graph.newTransaction();
-        GraphTraversalSource g = graph.traversal();
-        g.addV(JanusGraphDB.VL_MOVIE).property("movieId", 1,
+    public void addSingleNodeTest() throws Exception {
+        Vertex v1 = graph.addVertex(JanusGraphDB.VL_MOVIE);
+        v1.property("movieId", 11,
                 "title", "Midsummer",
                 "year", 1962,
                 "country", "UK",
                 "production", "Spyglass");
-        g.tx().commit();
-//        tx = graph.newTransaction();
-        Vertex v = g.V().hasLabel(JanusGraphDB.VL_MOVIE).next();
-        assertEquals("Retrieved MovieID", 1, v.property("movieId"));
+        Vertex v = graph.traversal().V().hasLabel(JanusGraphDB.VL_MOVIE).next();
+        assertEquals("Retrieved MovieID", "11", v.property("movieId").value());
     }
 
     @Test
